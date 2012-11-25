4ecf6d29ac0a60fd8cae0207894117075dde2c08
Radek Fryšták <geniv.radek@gmail.com>
2012-11-25 16:12:55 +0100

    uprava readme pridani nove sekce extenze a jeji naplneni

---
+++ part-2/4_extenze.php
@@ -0,0 +1,51 @@
+<?php
+
+# extenze pro usnadneni prace
+
+  # Dibi
+  # Dibi is Database Abstraction Library for PHP 5.
+  # quick via: http://dibiphp.com/cs/quick-start
+  # via: http://dibiphp.com/
+
+
+  # Smarty
+  # Smarty is a template engine for PHP
+  # via: http://www.smarty.net/
+
+
+  # phpmailer
+  # via: http://phpmailer.worxware.com/
+
+
+  # PHP Classes Repository
+  # via: http://www.phpclasses.org/
+
+
+  # Texy! je sexy!
+  # via: http://texy.info/cs/
+
+
+  # NotORM
+  # NotORM is a PHP library for simple working with data in the database. (by Jakub Vrana)
+  # via: http://www.notorm.com/
+
+
+  # Adminer
+  # Správa databáze v jednom PHP souboru. (by Jakub Vrana)
+  # via: http://www.adminer.org/
+
+
+  # Nette
+  # via: http://nette.org/cs/
+
+
+  # jQuery
+  # via: http://jquery.com/
+
+
+  # jQuery UI
+  # via: http://jqueryui.com/
+
+
+  # WYSIWYG Editor
+  # via: http://www.tinymce.com/tryit/full.php
