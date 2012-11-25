e9c745ba67b346235a3b1915521dc3617188ee71
Radek Fryšták <geniv.radek@gmail.com>
2012-11-25 19:41:29 +0100

    priprana a upravena prace s db a form

---
+++ part-2/7_form_db.php
@@ -0,0 +1,10 @@
+<?php
+
+# kombinace formulare a databaze (PDO)
+
+echo '
+<form method="post">
+  <input name="username" type="text" placeholder="sem prijde jmeno" />
+  <input name="email" type="text" placeholder="sem prijde email" />
+  <input name="tlacitko" type="submit" value="pridat" />
+</form>';
