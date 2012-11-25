4a150d60513d0ad2a7abeb9d03f7b49abea89d66
Radek Fryšták <geniv.radek@gmail.com>
2012-11-25 01:29:24 +0100

    funkcni model pridavani a mazani do db

--- part-2/3_databaze.php
+++ part-2/3_databaze.php
@@ -11,12 +11,44 @@
   try {
     //pripojeni
     $conn = new PDO('oci:dbname='.$host.';charset=UTF8', $user, $pswd);
-    var_dump($conn);
 
-    //
+    //insert
+    $sql = 'insert into users (username, email) values (?, ?)';
+    $state = $conn->prepare($sql);
+    var_dump($state->execute(array('franta', 'franta@email.cz')));
+
+    $tab = '<table border=1>
+    <tr>
+      <th>id</th>
+      <th>jmeno</th>
+      <th>email</th>
+    </tr>
+    ';
+
+    //select
+    $result = $conn->query('select * from users order by id');
+    if ($result) {
+      foreach($result as $row) {
+        $tab .= '<tr>
+        <td>'.$row['ID'].'</td>
+        <td>'.$row['USERNAME'].'</td>
+        <td>'.$row['EMAIL'].'</td>
+        </tr>';
+      }
+    }
+
+    $tab .= '
+    </table>';
+
+    echo $tab;
+
+    //delete
+    $sql = 'delete from users where username=?';
+    $state = $conn->prepare($sql);
+    var_dump($state->execute(array('franta')));
+
   } catch (PDOException $e) {
-    die("Failed to obtain database handle: " . $e->getMessage())
-
+    die("Failed to obtain database handle: " . $e->getMessage());
   }
 
 
